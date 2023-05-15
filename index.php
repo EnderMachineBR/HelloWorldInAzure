fcb15bf4162bd236a5844354f52e22f85972ccf7
Otávio Miquéias <116924249+EnderMachineBR@users.noreply.github.com>
2023-05-15 16:43:04 -0300

    Update index.php
    Atualização

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Me livrando da maldição 🗺️</title>
+    <title>Olá, Mundo! 🗺️</title>
 </head>
 <body>
     <?php 
-    echo "Hello World!"
+        echo "Olá, Mundo!"
     ?>
 </body>
 </html>
